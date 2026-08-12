99572215208fe38d0e03c551b32757bd52afeb08
SerhiiKI13 <kugai.sirozha@gmail.com>
2026-08-12 19:30:49 +0000

    L

---
+++ step2/week1/day5.py
@@ -0,0 +1,8 @@
+age = 23
+
+print(age > 18)
+print(age < 30)
+print(age == 23)
+print(age != 20)
+print(age >= 23)
+print(age <= 22)
---
+++ step2/week1/day6.py
@@ -0,0 +1,7 @@
+age = 23 
+has_license = True
+
+print(age > 18 and has_license == True)
+print(age < 18 or has_license == True)
+print(not has_license)
+print(age > 18 and age <= 30)
