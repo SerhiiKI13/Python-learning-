aa17f155778b8c02480f8242fadf092a30819a06
SerhiiKI13 <kugai.sirozha@gmail.com>
2026-08-12 17:26:25 +0000

    Trsiningit commit -m Training day 1

---
+++ step2/week1/day1.py
@@ -0,0 +1,11 @@
+name = "Serhii"
+age = 23
+height = 185
+isStudent = True
+isNone = None
+
+print(name, type(name))
+print(age, type(age))
+print(height, type(height))
+print(isStudent, type(isStudent))
+print(isNone, type(isNone))
